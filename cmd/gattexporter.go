10f76eb6a85ae2d0f4b882f314777908f7813895
VALERE91 <valereplantevin@gmail.com>
2016-07-07 18:53:41 -0400

    Add basic exporter

--- cmd/gattexporter.go
+++ cmd/gattexporter.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/VALERE91/GATTExporter"
+)
 
 func main() {
-	fmt.Println("Welcome to GATT exporter")
+	fmt.Println("Welcome to GATT exporter!!!!")
+	gattexporter.Exporter.Start()
 }
--- exporter.go
+++ exporter.go
@@ -1,1 +1,50 @@
-package GATTExporter
+package gattexporter
+
+import (
+	"os"
+	"bufio"
+	"strings"
+)
+
+type exporter struct{
+	reader *bufio.Reader
+	writer *bufio.Writer
+}
+
+var Exporter = exporter{
+	reader:bufio.NewReader(os.Stdin),
+	writer:bufio.NewWriter(os.Stdout),
+}
+
+func(exporter *exporter) Start() {
+	text, err := Exporter.reader.ReadString('\n')
+
+	for err == nil{
+		Exporter.interpretCommand(text)
+		text, err = Exporter.reader.ReadString('\n')
+	}
+}
+
+func(exporter *exporter) interpretCommand(command string) {
+	parts := strings.Split(command, " ")
+
+	switch parts[0] {
+	case "CONNECT":
+		if len(parts) == 2{
+
+		}else{
+			Exporter.writer.WriteString("Usage : CONNECT XX:XX:XX:XX:XX:XX")
+		}
+		break
+	case "DISCONNECT":
+		break
+	case "LIST":
+		if len(parts) == 2{
+
+		}else{
+			Exporter.writer.WriteString("Usage : LIST TIMEOUT_IN_SEC")
+		}
+		break
+	}
+}
+
